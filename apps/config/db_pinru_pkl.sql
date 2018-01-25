425d9f75200ff7415a18d842f72023e39c098c16
oimtrust <trustoim@gmail.com>
2018-01-25 13:31:00 +0700

    first upload

---
+++ apps/config/db_pinru_pkl.sql
@@ -0,0 +1,166 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.6
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jan 25, 2018 at 01:28 PM
+-- Server version: 5.7.20-0ubuntu0.16.04.1
+-- PHP Version: 7.0.22-0ubuntu0.16.04.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_pinru_pkl`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_fakultas`
+--
+
+CREATE TABLE `tbl_fakultas` (
+  `id_fakultas` int(11) NOT NULL,
+  `nama_fakultas` varchar(100) DEFAULT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_peminjaman`
+--
+
+CREATE TABLE `tbl_peminjaman` (
+  `id_peminjaman` int(11) NOT NULL,
+  `id_user` int(15) NOT NULL,
+  `id_ruang` int(11) NOT NULL,
+  `tgl_awal` date DEFAULT NULL,
+  `tgl_akhir` date NOT NULL,
+  `jam_awal` time NOT NULL,
+  `jam_akhir` time NOT NULL,
+  `status` varchar(20) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_prodi`
+--
+
+CREATE TABLE `tbl_prodi` (
+  `id_prodi` int(11) NOT NULL,
+  `nama_prodi` varchar(100) NOT NULL,
+  `id_fakultas` int(11) NOT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_ruang`
+--
+
+CREATE TABLE `tbl_ruang` (
+  `id_ruang` int(11) NOT NULL,
+  `nama_ruang` varchar(100) DEFAULT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `id_user` int(15) NOT NULL,
+  `nama_user` varchar(100) NOT NULL,
+  `id_fakultas` int(11) NOT NULL,
+  `id_prodi` int(11) NOT NULL,
+  `alamat` text NOT NULL,
+  `status` varchar(15) DEFAULT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_fakultas`
+--
+ALTER TABLE `tbl_fakultas`
+  ADD PRIMARY KEY (`id_fakultas`);
+
+--
+-- Indexes for table `tbl_peminjaman`
+--
+ALTER TABLE `tbl_peminjaman`
+  ADD PRIMARY KEY (`id_peminjaman`);
+
+--
+-- Indexes for table `tbl_prodi`
+--
+ALTER TABLE `tbl_prodi`
+  ADD PRIMARY KEY (`id_prodi`);
+
+--
+-- Indexes for table `tbl_ruang`
+--
+ALTER TABLE `tbl_ruang`
+  ADD PRIMARY KEY (`id_ruang`);
+
+--
+-- Indexes for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_fakultas`
+--
+ALTER TABLE `tbl_fakultas`
+  MODIFY `id_fakultas` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_peminjaman`
+--
+ALTER TABLE `tbl_peminjaman`
+  MODIFY `id_peminjaman` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_prodi`
+--
+ALTER TABLE `tbl_prodi`
+  MODIFY `id_prodi` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_ruang`
+--
+ALTER TABLE `tbl_ruang`
+  MODIFY `id_ruang` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ apps/model/Connection.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Connection;
+
+class Connection
+{
+    
+}
