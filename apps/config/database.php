ded203bc7d2e2eb8054d3ec83c3da44c6a932c83
hakiki95 <hakiki95com@gmail.com>
2018-02-24 11:17:13 +0700

    local 95

--- apps/config/database.php
+++ apps/config/database.php
@@ -1,5 +1,5 @@
 <?php
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'root');
-define('DB_DATABASE', 'db_pinru_pkl');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'pinru');
